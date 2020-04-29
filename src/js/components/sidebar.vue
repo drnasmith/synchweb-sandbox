1f58336a6c491c2291a6493bbf51ed8ffb606b51
Neil A Smith <dr.neil.a.smith@gmail.com>
2020-04-29 10:20:48 +0100

    Sidebar gets menu from prop

--- src/js/components/sidebar.vue
+++ src/js/components/sidebar.vue
@@ -1,26 +1,29 @@
 <template>
 
-    <div v-show="isLoggedIn" class="tw-flex tw-flex-col tw-bg-sidebar-grad-end md:tw-bg-gray-200 md:tw-flex-row tw-justify-center tw-my-2 md:tw-mx-4">
+    <div v-show="isLoggedIn" class="tw-flex tw-flex-col tw-bg-sidebar-grad-end md:tw-flex-row tw-justify-center tw-my-2 md:tw-mx-4">
         <router-link to="/proposals" class="md:tw-w-48 tw-py-4 tw-bg-sidebar-grad-end hover:tw-bg-gray-100 tw-border tw-border-gray-400 tw-text-gray-800 tw-text-center tw-text-xs">
             Proposals
         </router-link>
 
-        <a href="#" class="tw-relative md:tw-w-48 tw-py-4 tw-bg-sidebar-grad-end tw-border tw-border-gray-400 tw-text-gray-800 tw-text-center tw-text-xs"
+        <a href="#" class="tw-relative md:tw-w-48 tw-py-4 tw-bg-sidebar-grad-end hover:tw-bg-gray-100 tw-border tw-border-gray-400 tw-text-gray-800 tw-text-center tw-text-xs"
           @mouseover="showProposalMenu = true" @mouseleave="showProposalMenu=false" @click="showProposalMenu=false">
             <p class="tw-text-xs">{{proposal ? proposal : 'No Proposal'}} <i v-show="proposal" class="fa fa-chevron-down"/></p>
 
             <div v-show="showProposalMenu && proposal">
                 <div class="tw-absolute tw-left-0 tw-mt-4 tw-w-full">
                     <ul class="tw-border-t tw-border-l tw-border-r">
-                        <li class="tw-w-full tw-border-b tw-border-gray-400 hover:tw-text-white tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
+                        <li v-for="(item, index) in menu" :key="index" class="tw-w-full tw-border-b tw-border-gray-400 tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
+                            <router-link  :to="item.link" class="tw-block tw-text-gray-800 tw-py-4">{{item.name}}</router-link>
+                        </li>
+                        <!-- <li class="tw-w-full tw-border-b tw-border-gray-400 tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
                             <router-link  to="/shipments" class="tw-block tw-text-gray-800 tw-py-4">Shipments</router-link>
                         </li>
-                        <li class="tw-w-full tw-border-b tw-border-gray-400 hover:tw-text-white tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
+                        <li class="tw-w-full tw-border-b tw-border-gray-400 tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
                             <router-link  to="/proteins" class="tw-block tw-text-gray-800 tw-py-4">Proteins</router-link>
                         </li>
-                        <li class="tw-w-full tw-border-b tw-border-gray-400 hover:tw-text-white tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
+                        <li class="tw-w-full tw-border-b tw-border-gray-400 tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
                             <router-link  to="/contacts" class="tw-block tw-text-gray-800 tw-py-4">Contacts</router-link>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
             </div>
@@ -46,7 +49,7 @@
             <div v-show="showHelpMenu" >
                 <div class="tw-absolute tw-left-0 tw-mt-4 tw-w-full">
                     <ul class="tw-border-t tw-border-l tw-border-r">
-                        <li class="tw-w-full tw-border-b tw-border-gray-400 hover:tw-text-white tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
+                        <li class="tw-w-full tw-border-b tw-border-gray-400 tw-bg-sidebar-grad-end  hover:tw-bg-blue-400">
                             <router-link  to="/docs" class="tw-block tw-py-4 tw-text-gray-800 ">Tutorials</router-link>
                         </li>
                     </ul>
